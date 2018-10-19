9d3c530abcae1392f955bc390a5474ac24565fce
coltaemanuela <coltaemanuela@gmail.com>
2018-10-20 00:25:21 +0100

    PerC- initial commit with transactions list as JSON

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const config = require('./config.json')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+  axios.get('https://api.monzo.com/transactions', {
+    headers: {
+      Authorization: `Bearer ${config.apiKey}`
+    },
+    params: {
+      account_id: config.accountId
+    }
+  }).then(response => {
+    const cleanList = response.data.transactions
+        .map(t => {
+          return {
+            amount: t.amount,
+            date: t.created
+          };
+        });
+    res.send(JSON.stringify(cleanList));
+    next();
+  }).catch(error => {
+    console.log(error);
+    next();
+  })
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
