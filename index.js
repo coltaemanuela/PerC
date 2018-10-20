b9ed1291488405d28a7a6136a54a268a32eeac10
coltaemanuela <coltaemanuela@gmail.com>
2018-10-20 08:33:13 +0100

    add pagination by date

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config.json')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res, next) => {
+app.get('/api/transactions', (req, res, next) => {
   axios.get('https://api.monzo.com/transactions', {
     headers: {
       Authorization: `Bearer ${config.apiKey}`
@@ -16,6 +16,7 @@ app.get('/', (req, res, next) => {
     const cleanList = response.data.transactions
         .map(t => {
           return {
+            id:t.id,
             amount: t.amount,
             date: t.created,
             category: t.category
@@ -29,5 +30,27 @@ app.get('/', (req, res, next) => {
   })
 });
 
+/**
+ * Identify the weekly expenses 
+ * Current version is just for testing purposes. 
+ * The query param needs to be replaced with the id sent by POST or, better, take the date now - 7 and run a cron job ??
+*/
+app.get('/api/weekly-expenses/:date', (req, res, next) => {
+  axios.get('https://api.monzo.com/transactions', {
+        headers: {
+          Authorization: `Bearer ${config.apiKey}`
+        },
+        params: {
+          account_id: config.accountId,
+          since: ""+ req.params.date+"" 
+        }
+      }).then(response => {
+        const cleanList = response.data.transactions;
+        const donation = cleanList.map(t => {
+            return Math.abs(t.amount)*0.1;            
+        });
+        res.send(JSON.stringify(donation));         
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
