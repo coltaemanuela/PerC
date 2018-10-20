5f15ff84dd48c2d97e22f5df1c6c2f5f5dc4ca4b
Alasdair Munday <alasdair.munday@googlemail.com>
2018-10-20 07:44:02 +0100

    encapsulated into files

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-const config = require('./config.json')
+const config = require('./config.json');
+const postToFeed = require('./actions/postToFeed');
+const calculatePledge = require('./actions/calculatePledge')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res, next) => {
-  axios.get('https://api.monzo.com/transactions', {
-    headers: {
-      Authorization: `Bearer ${config.apiKey}`
-    },
-    params: {
-      account_id: config.accountId
-    }
-  }).then(response => {
-    const cleanList = response.data.transactions
-        .map(t => {
-          return {
-            amount: t.amount,
-            date: t.created,
-            category: t.category
-          };
-        });
-    res.send(JSON.stringify(cleanList));
-    next();
-  }).catch(error => {
-    console.log(error);
-    next();
-  })
-});
+app.get('/', calculatePledge);
 
+app.get('/give/:amount', postToFeed);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
