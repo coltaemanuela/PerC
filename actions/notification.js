49460879fe29db5c2a9b7c067620937f047d1ba5
Alasdair Munday <alasdair.munday@googlemail.com>
2018-10-21 16:19:48 +0100

    used node notifier

--- actions/notification.js
+++ actions/notification.js
@@ -1,11 +1,18 @@
 const qs = require('qs');
 const axios = require('axios');
 const config = require('../config.json');
-const notifier = require('node-notifier');
+const NotificationCenter = require('node-notifier').NotificationCenter;
+
+var notifier = new NotificationCenter({
+  withFallback: false, // Use Growl Fallback if <= 10.8
+  customPath: void 0 // Relative/Absolute path to binary if you want to use your own fork of terminal-notifier
+});
 
 module.exports = (amount) => {
     notifier.notify({
         title: 'Your PerC donation is ready',
-        message: 'Your suggested donation is ready to be confirmed'
+        message: 'Your suggested donation is ready to be confirmed',
+        open: "http://localhost:3001/demos/008-theme061.html",
+        actions: "open"
     });
 };
--- index.js
+++ index.js
@@ -81,23 +81,11 @@ app.get('/weekly-donation', (req,res,next) =>  {
   });
 });
 
-// app.get('/notification', function(req, res) {
-//   webpush.setVapidDetails(
-//     'mailto:alasdair.munday@gmail.com',
-//     config.vapidKeys.public,
-//     config.vapidKeys.private
-//   );
-//   webpush.sendNotification({
-//     "endpoint":"https://updates.push.services.mozilla.com/wpush/v1/gAAAAABbyxKS6v6Rqhm-BO3RgmLK92qTvLOuXZN1f6dw5FKYnHnndXTpihbi3Z1n65xvSLwWWRSejIVIcyr_OX8JhBL7a9NUQh-XmQ_y-8mQ3RrMYTF1x4YdQlAXJg8tNMo9nfuq-7Dc",
-//     "keys":{"auth":"i2L5fHKaeY6eAYFq2em36w","p256dh":"BC_4NZ81cetiv08GR5I_tJNCglSgXTiDNTMSYf0DL1EySBwmSN7zY3GPNJxRdXEmKyf0uIahHrHTrA_EMgi9lpM"}},
-//     'Your Push Payload Text');
-//   res.sendStatus(200);
-// });
-
 app.get('/pledge', calculatePledge);
 
 app.get('/notification', (req,res) => {
-  notification()
+  notification();
+  res.sendStatus(200);
 });
 
 app.get('/give/:amount', postToFeed);
